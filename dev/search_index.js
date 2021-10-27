6848af2df49bf195840ba73e25cb71c459a9c81f
Documenter.jl <documenter@juliadocs.github.io>
2021-10-27 15:43:21 +0000

    build based on 9f1c377

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Bonobo","category":"page"},{"location":"#Bonobo","page":"Home","title":"Bonobo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Bonobo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Bonobo]","category":"page"}]
+}
